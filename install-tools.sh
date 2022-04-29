56364755936d8bb66a13afc7ce98be54f1b254d1
Sven Erik Knop <seknop@gmail.com>
2022-04-29 20:32:53 +0100

    Also provide confluent.tar as well as krb5-user

--- install-tools.sh
+++ install-tools.sh
@@ -8,7 +8,17 @@ wget -qO - https://packages.confluent.io/deb/7.1/archive.key | sudo apt-key add 
 sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/7.1 stable main"
 sudo add-apt-repository "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main"
 
-sudo apt-get update && \
-sudo apt-get install -y confluent-platform && \
-sudo apt-get install -y confluent-security
+sudo apt update && \
+sudo apt install -y confluent-platform && \
+sudo apt install -y confluent-security
 
+# some other useful tools
+sudo DEBIAN_FRONTEND=noninteractive apt install -y krb5-user
+
+# drop in confluent archive at the correct location for offline installs
+
+sudo mkdir -p /opt/confluent
+cd /opt/confluent
+sudo curl -O http://packages.confluent.io/archive/7.1/confluent-7.1.1.tar.gz
+sudo tar xvf confluent-7.1.1.tar.gz
+sudo rm confluent-7.1.1.tar.gz
