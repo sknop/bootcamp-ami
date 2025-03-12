d6046b713aadc0b4afde69a1ae18903f76600a79
Sven Erik Knop <seknop@gmail.com>
2025-03-12 10:31:39 +0000

    Update to CP 7.9 and Ubuntu 24.04 (second attempt)

--- install-tools.sh
+++ install-tools.sh
@@ -8,9 +8,22 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openjdk-17-jdk
 
 echo "Installing CP version ${version}"
 
-wget -qO - https://packages.confluent.io/deb/${version}/archive.key | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/${version} stable main"
-sudo add-apt-repository "deb https://packages.confluent.io/clients/deb $(lsb_release -cs) main"
+mkdir -p /etc/apt/keyrings
+wget -qO - https://packages.confluent.io/deb/7.9/archive.key | gpg --dearmor | sudo tee /etc/apt/keyrings/confluent.gpg > /dev/null
+
+echo "Types: deb
+URIs: https://packages.confluent.io/deb/${version}
+Suites: stable
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/confluent.gpg
+
+Types: deb
+URIs: https://packages.confluent.io/clients/deb
+Suites: $(lsb_release -cs)
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/confluent.gpg" | sudo tee /etc/apt/sources.list.d/confluent-platform.sources > /dev/null
 
 sudo apt-get update && \
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y confluent-platform && \
